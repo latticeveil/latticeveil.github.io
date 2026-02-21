98baa179e0c2f4dd4ff3cf81758fb2393b00c204
Redacted480 <dionshepard0@gmail.com>
2026-02-21 15:10:15 -0500

    Update Supabase anon key to correct dashboard key

--- veilnet/assets/config.js
+++ veilnet/assets/config.js
@@ -1,6 +1,6 @@
 window.VEILNET_CONFIG = {
   SUPABASE_URL: "https://lqghurvonrvrxfwjgkuu.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxZ2h1cnZvbnJ2cnh3amtrdXUiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTczODI4MjQzNSwiZXhwIjoyMDUzODU4NDM1fQ.q3q2i8t9RrVGdYpN3Z4X9Y3k9L8p1n6v7g8h2f1k3m4",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxZ2h1cnZvbnJ2cnhmd2pna3V1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyODA1OTMsImV4cCI6MjA4Njg1NjU5M30.TUulKqgdUdoaYn9O5SKOJfh61DLprLTvf2fU_9CNF_U",
   GOOGLE_CLIENT_ID: "843782276052-2drjqat4c5qqg36cd1gi25jd9sf0pn8s.apps.googleusercontent.com",
   PROFILE_TABLE: "profiles",
   BASE_PATH: "/veilnet",
