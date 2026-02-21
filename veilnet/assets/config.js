54dde02b9786f78a85b3bcf6801be59143231a05
Redacted480 <dionshepard0@gmail.com>
2026-02-21 15:03:09 -0500

    Fix Supabase anon key

--- veilnet/assets/config.js
+++ veilnet/assets/config.js
@@ -1,6 +1,6 @@
 window.VEILNET_CONFIG = {
   SUPABASE_URL: "https://lqghurvonrvrxfwjgkuu.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxZ2h1cnZvbnJ2cnhmd2pna3V1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyODA1OTMsImV4cCI6MjA4Njg1NjU5M30.TUulKqgdUdoaYn9O5SKOJfh61DLprLTvf2fU_9CNF_U",
+  SUPABASE_ANON_KEY: "f2a649829f2108e70a0c4ca765f73d9d43950e4e48c3435e346b03de7e54c5fc",
   GOOGLE_CLIENT_ID: "843782276052-2drjqat4c5qqg36cd1gi25jd9sf0pn8s.apps.googleusercontent.com",
   PROFILE_TABLE: "profiles",
   BASE_PATH: "/veilnet",
