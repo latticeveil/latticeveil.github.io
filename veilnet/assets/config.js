34629f644bb8d69d495ab5ce1eaa0aa068b21457
Redacted480 <dionshepard0@gmail.com>
2026-02-21 15:05:25 -0500

    Revert to JWT anon key

--- veilnet/assets/config.js
+++ veilnet/assets/config.js
@@ -1,6 +1,6 @@
 window.VEILNET_CONFIG = {
   SUPABASE_URL: "https://lqghurvonrvrxfwjgkuu.supabase.co",
-  SUPABASE_ANON_KEY: "f2a649829f2108e70a0c4ca765f73d9d43950e4e48c3435e346b03de7e54c5fc",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxZ2h1cnZvbnJ2cnh3amtrdXUiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTczODI4MjQzNSwiZXhwIjoyMDUzODU4NDM1fQ.q3q2i8t9RrVGdYpN3Z4X9Y3k9L8p1n6v7g8h2f1k3m4",
   GOOGLE_CLIENT_ID: "843782276052-2drjqat4c5qqg36cd1gi25jd9sf0pn8s.apps.googleusercontent.com",
   PROFILE_TABLE: "profiles",
   BASE_PATH: "/veilnet",
